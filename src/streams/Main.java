3605bb3b1639785d5ec5ec3b96d1b2932fb98833
Liel Ran <liel.ran@hibob.io>
2016-08-15 22:54:06 +0300

    parallel stream

--- src/streams/Main.java
+++ src/streams/Main.java
@@ -6,6 +6,15 @@ import java.util.List;
 /**
  * Created by lielran on 8/15/16.
  */
+
+/**
+ * output:
+ *
+ * 1 (streams)
+ * 2 (parallel Stream)
+ *
+ *
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -42,6 +51,13 @@ public class Main {
                 .count();
 
         System.out.println(closedAndPositive);
+
+        long positive =  trades.parallelStream()
+                .filter(t -> t.getTotalAmount() >0)
+                .count();
+
+        System.out.println(positive);
+
     }
 
 }
